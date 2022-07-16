7da7b10255e6eff781ad68ce34b2736df3c0dbd6
tarkangel <angel_calderon5@hotmail.com>
2022-07-16 04:40:32 -0500

    Added program.c

---
+++ program.cs
@@ -0,0 +1,14 @@
+    using System;
+
+namespace helloWorld
+
+{
+    class FirstClass    
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World");
+
+            }
+        }
+}
